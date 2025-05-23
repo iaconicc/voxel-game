f8b89393bf46e4f6b5e331b92c7204033800e748
Rehoboth Lenin <rehoboth01@outlook.com>
2025-05-23 13:48:44 +0100

    vertex definition

--- voxel game/DX3D11.h
+++ voxel game/DX3D11.h
@@ -2,6 +2,12 @@
 #include <stdbool.h>
 #include <d3d11.h>
 
+typedef struct {
+	int x;
+	int y;
+	int z;
+}vertex;
+
 void CreateDX3D11DeviceForWindow(HWND hwnd);
 void DestroyDX3D11DeviceForWindow();
 void EndFrame();
---
+++ voxel game/chunk.c
@@ -0,0 +1,1 @@
+#include "DX3D11.h"
