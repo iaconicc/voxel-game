535b46f184ece1f857cd28fa7ddde282e10aa027
Rehoboth Lenin <rehoboth01@outlook.com>
2025-05-23 14:13:47 +0100

    cubeFaces defined

--- voxel game/chunk.c
+++ voxel game/chunk.c
@@ -1,5 +1,9 @@
 #include "DX3D11.h"
 
+typedef struct {
+	int index[6];
+}face;
+
 static vertex cubeVertexs[8] = {
 	{0.0f, 0.0f, 0.0f},
 	{1.0f, 0.0f, 0.0f},
@@ -10,3 +14,17 @@ static vertex cubeVertexs[8] = {
 	{1.0f, 1.0f, 1.0f},
 	{0.0f, 1.0f, 1.0f},
 };
+
+static face cubeFaces[6] = {
+	{0, 3, 1, 1, 3, 2}, //back face
+	{5, 6, 4, 4, 6, 7}, //front face
+	{3, 7, 2, 2, 7, 6}, //top face
+	{1, 5, 0, 0, 5, 4}, //bottom face
+	{4, 7, 0, 0, 7, 3}, //Left face
+	{1, 2, 5, 5, 2, 6}, //Right face
+};
+
+createBlock()
+{
+
+}
