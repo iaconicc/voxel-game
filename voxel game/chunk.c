beff338d8450a4ad4e9d39557cbdee9d925bf74e
Rehoboth Lenin <rehoboth01@outlook.com>
2025-05-25 09:19:13 +0100

    chunk face culling

--- voxel game/chunk.c
+++ voxel game/chunk.c
@@ -26,6 +26,15 @@ const static face cubeFaces[6] = {
 	{1, 2, 5, 5, 2, 6}, //east face
 };
 
+const static vec3 faceChecks[6] = {
+	{0.0f,0.0f,-1.0f},
+	{0.0f,0.0f,1.0f},
+	{0.0f,1.0f,0.0f},
+	{0.0f,-1.0f,0.0f},
+	{-1.0f,0.0f,0.0f},
+	{1.0f,0.0f,0.0f},
+};
+
 vec3* vertexlist;
 int* indexlist;
 
@@ -40,26 +49,66 @@ typedef struct {
 	Block blocks[CHUNK_SIZE][CHUNK_SIZEV][CHUNK_SIZE];
 }Chunk;
 
+Chunk chunk;
+
+static void populateVoxelMap(){
+	for (size_t x = 0; x < CHUNK_SIZE; x++)
+	{
+		for (size_t y = 0; y < CHUNK_SIZEV; y++)
+		{
+			for (size_t z = 0; z < CHUNK_SIZE; z++)
+			{
+				chunk.blocks[x][y][z].isAir = false;
+			}
+		}
+	}
+}
+
+static bool checkVoxel(vec3 pos)
+{
+	int x =(int) floorf(pos[0]);
+	int y = (int)floorf(pos[1]);
+	int z = (int)floorf(pos[2]);
+
+	if (x < 0 || x > CHUNK_SIZE - 1 || y < 0 || y > CHUNK_SIZEV - 1 || z < 0 || z > CHUNK_SIZE - 1)
+	{
+		return true;
+	}
+
+	return chunk.blocks[x][y][z].isAir;
+}
+
 int currentVertexindex = 0;
 
 static void addVoxelDataToChunk(vec3 pos)
 {
-	for (size_t f = 0; f < 6; f++)
-	{
-		for (size_t v = 0; v < 6; v++)
+	
+		for (size_t f = 0; f < 6; f++)
 		{
-			int FaceIndex = cubeFaces[f].index[v];
-			glm_vec3_add(cubeVertexs[FaceIndex], pos, vertexlist[currentVertexindex]);
-			indexlist[currentVertexindex] = currentVertexindex;
-			currentVertexindex++;
+			vec3 blockTocheck;
+			glm_vec3_add(pos, faceChecks[f], blockTocheck);
+
+			if (checkVoxel(blockTocheck)) {
+				for (size_t v = 0; v < 6; v++)
+				{
+					int FaceIndex = cubeFaces[f].index[v];
+					glm_vec3_add(cubeVertexs[FaceIndex], pos, vertexlist[currentVertexindex]);
+					indexlist[currentVertexindex] = currentVertexindex;
+					currentVertexindex++;
+				}
+			}
 		}
-	}
+	
 }
 
 void createBlock()
 {
 	int indexSize = 0;
 	int vertexSize = 0;
+
+	//vertexSize = 36 * CHUNK_SIZE * CHUNK_SIZEV * CHUNK_SIZE; // 6 faces, 6 vertices per face
+	//indexSize = 36 * CHUNK_SIZE * CHUNK_SIZEV * CHUNK_SIZE;
+
 	//check each face and calculate how large the vertex and index list should be
 	for (size_t x = 0; x < CHUNK_SIZE; x++)
 	{
@@ -67,21 +116,26 @@ void createBlock()
 		{
 			for (size_t z = 0; z < CHUNK_SIZE; z++)
 			{
-				indexSize += 6;
-				vertexSize += 8;
+				for (size_t f = 0; f < 6; f++)
+				{
+					vec3 pos = {x,y,z};
+					vec3 blockToCheck;
+					glm_vec3_add(pos, faceChecks[f], blockToCheck);
+					if (checkVoxel(blockToCheck))
+					{
+						indexSize += 6;
+						vertexSize += 6;
+					}
+				}
 			}
 		}
 	}
-
-	indexSize *= 6;
-	vertexSize *= 8;
 
 	//allocate memory enough for both indices and vertexes
 	indexlist = malloc(sizeof(int) * indexSize);
 	vertexlist = malloc(sizeof(vec3) * vertexSize);
 
 	//add vertexs and indices
-
 	for (size_t x = 0; x < CHUNK_SIZE; x++)
 	{
 		for (size_t y = 0; y < CHUNK_SIZEV; y++)
@@ -93,7 +147,6 @@ void createBlock()
 			}
 		}
 	}
-
 
 	createVertexBufferAndAppendToList(vertexlist, sizeof(vec3) * vertexSize);
 	createIndexDataBuffer(indexlist, sizeof(int) * indexSize, indexSize);
